77350028dff2de36be4373b541d45169df782a69
Andriy Dzyunyak <andriy.dzyunyak@gmail.com>
2022-01-15 01:52:05 +0200

    add task 1 - color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+let intervalId = null;
 
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+refs.startBtn.addEventListener('click', onBtnChangeColorStart);
+refs.stopBtn.addEventListener('click', onBtnChangeColorStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onBtnChangeColorStart() {
+  intervalId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+    refs.startBtn.setAttribute('disabled', true);
+  }, 1000);
+}
+
+function onBtnChangeColorStop() {
+  clearInterval(intervalId);
+  refs.startBtn.removeAttribute('disabled');
+}
